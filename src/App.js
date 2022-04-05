741b7790dd5631662a4d1094417c0382bef4b607
alexandermaxwellogle <maxogle@gmail.com>
2022-04-05 12:23:18 -0400

    added props to app.js, styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
+import Fighter from './components/Fighter';
 
 function App() {
   return (
     <div className="App">
       <h1>Fighters</h1>
       <div className="fighters-grid">
-        
+        <Fighter name={'Roy'} color={'red'}/>
+        <Fighter name={'fox'} color={'green'}/>
+        <Fighter name={'Ganondorf'} color={'purple'} />
       </div>
     </div>
   );
