3f78bdde7ec9da404b9e8f1a328111393046ba02
alexandermaxwellogle <maxogle@gmail.com>
2022-04-05 13:14:55 -0400

    created fighter screen

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ function App() {
         <Fighter name={'Roy'} color={'red'} img={''}/>
         <Fighter name={'fox'} color={'green'} img={''}/>
         <Fighter name={'Ganondorf'} color={'purple'} img={''} />
+        <Fighter name={'sheik'} color={'lightskyblue'} img={''} />
+      </div>
+      <div className='fighter-screen'>
+        <h2>Roy</h2>
+        <img src={'https://www.smashbros.com/assets_v2/img/fighter/roy/main.png'} />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, sint magni. Consequuntur neque repellat maxime asperiores, iusto reiciendis voluptate nobis fugiat tempora facere labore doloremque dolor ratione a facilis quia.</p>
       </div>
     </div>
   );
