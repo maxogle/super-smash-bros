b3628868fdf6e9bbb8ca46453421b5ae1cedb2e2
alexandermaxwellogle <maxogle@gmail.com>
2022-04-05 17:55:46 -0400

    its fucked

--- src/components/Fighter.js
+++ src/components/Fighter.js
@@ -1,11 +1,14 @@
 
 
 
-export default ({name, color, setVisible }) => {
+export default ({name, color, setVisible, setSelectedCharacter}) => {
     return (
         <div 
-        onClick={() => setVisible(true)}
-        className="fighter" style={{backgroundColor: color, backgroundImage: `url(https://www.smashbros.com/assets_v2/img/fighter/${name.toLowerCase()}/main.png)`}}> 
+        onClick={() => {
+            setSelectedCharacter({name: name, color: color})
+            setVisible(true)
+        }}
+        className="fighter" style={{backgroundColor: color, backgroundImage: `url(https://www.smashbros.com/assets_v2/img/fighter/${selectedCharacter.name.toLowerCase()}/main.png)`}}> 
             <h4>{name}</h4>
         </div>
     )
--- src/components/FighterScreen.js
+++ src/components/FighterScreen.js
@@ -1,9 +1,11 @@
-const FighterScreen = ({isVisible, setVisible}) => {
+const FighterScreen = ({isVisible, setVisible, selectedCharacter}) => {
     return (
         <div className='fighter-screen' style={{display : isVisible ? "block" : "none"}}>
-            <h2>Roy</h2>
-            <img width="100" height="100" src={'https://www.smashbros.com/assets_v2/img/fighter/roy/main.png'} />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, sint magni. Consequuntur neque repellat maxime asperiores, iusto reiciendis voluptate nobis fugiat tempora facere labore doloremque dolor ratione a facilis quia.</p>
+            <div onClick={() => { setVisible(false) }}>               
+                <h2>Roy</h2>
+                <img width="100" height="100" src={`https://www.smashbros.com/assets_v2/img/fighter/${selectedCharacter.name.toLowerCase()}/main.png`} />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, sint magni. Consequuntur neque repellat maxime asperiores, iusto reiciendis voluptate nobis fugiat tempora facere labore doloremque dolor ratione a facilis quia.</p>
+            </div>
         </div>
     )
 }
